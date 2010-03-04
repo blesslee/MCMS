91f58ac90ad5143bbf7eb791ebfd185665deaa14
blesslee <ltyou@msn.com>
2010-03-03 20:39:21 -0500

    Add initial project.clj's for client and server

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject MCMS "0.1.0-SNAPSHOT"
+ :description "A media collection manager."
+ :dependencies [[fleetdb "0.1.1-SNAPSHOT"]]
+ :dev-dependencies [[fleetdb-client "0.1.1-SNAPSHOT"]])
---
+++ server/project.clj
@@ -0,0 +1,6 @@
+(defproject MCMS "0.1.0-SNAPSHOT"
+ :description "A media collection manager."
+ :main fleetdb.server
+ :dependencies [[fleetdb "0.1.1-SNAPSHOT"]
+                [leiningen-run "0.2"]]
+ )
